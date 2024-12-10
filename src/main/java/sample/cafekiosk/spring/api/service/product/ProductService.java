02de83107a8e4c0b3293d152672175a777c58d85
ryu-hyewon <lyu9801@gmail.com>
2024-12-10 23:26:34 +0900

    Practical Testing 프로젝트 first commit

---
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -0,0 +1,60 @@
+package sample.cafekiosk.spring.api.service.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+
+/**
+ * readOnly = true : 읽기전용
+ * CRUD 에서 CUD 동작 X / only Read
+ * JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+ *
+ * CQRS - Command / Query 분리
+ */
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if(latestProductNumber == null) {
+            return "001";
+        }
+
+        int lastestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = lastestProductNumberInt + 1;
+
+        // 9 -> 009
+        return String.format("%03d", nextProductNumberInt);
+    }
+
+    public List<ProductResponse> getSellingProducts() {
+        List<Product> products = productRepository.findAllBySellingStatusIn(forDisplay());
+
+        return products.stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+}
